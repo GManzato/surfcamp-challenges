d6787e8279aa8a2d9bc9dc6f6cb3d873df5739a4
Gilles Manzato <gilles.manzato@gmail.com>
2014-09-10 17:00:56 +0200

    WIP 03-03

--- Advanced/01-Ruby/03-Hash-Symbols/03-Numbers-and-Letters/lib/longest_word.rb
+++ Advanced/01-Ruby/03-Hash-Symbols/03-Numbers-and-Letters/lib/longest_word.rb
@@ -3,8 +3,21 @@ require 'json'
 
 def generate_grid(grid_size)
   #TODO: generate random grid of letters
+  #('a'..'z').to_a.sample(grid_size)
+  [*('a'..'z'),*('a'..'z')].sample(grid_size)
 end
 
 def run_game(attempt, grid, start_time, end_time)
   #TODO: runs the game and return detailed hash of result
+  api_url = "http://api.wordreference.com/0.8/80143/json/enfr/" + attempt
+	open(api_url) do |stream| 
+	  response = JSON.parse(stream.read)
+	  if response['Error']
+	  	message = "Mot invalide"
+		else 
+			puts response['term0']['PrincipalTranslations']['0']['FirstTranslation']['term']
+		end
+	  
+	  #puts response['term0']['PrincipalTranslations'][0]['FirstTranslation']
+	end
 end
