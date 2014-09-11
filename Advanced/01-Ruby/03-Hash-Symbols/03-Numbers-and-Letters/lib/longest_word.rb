3e2d48af459f19ad760ec58932f15928e2bec045
Gilles Manzato <gilles.manzato@gmail.com>
2014-09-11 10:47:09 +0200

    Finish 03-03

--- Advanced/01-Ruby/03-Hash-Symbols/03-Numbers-and-Letters/lib/longest_word.rb
+++ Advanced/01-Ruby/03-Hash-Symbols/03-Numbers-and-Letters/lib/longest_word.rb
@@ -3,21 +3,40 @@ require 'json'
 
 def generate_grid(grid_size)
   #TODO: generate random grid of letters
-  #('a'..'z').to_a.sample(grid_size)
-  [*('a'..'z'),*('a'..'z')].sample(grid_size)
+  [*('A'..'Z'),*('A'..'Z')].sample(grid_size)
+end
+
+def in_the_grid?(attempt,grid)
+	#grid.all? {|letter| attempt.include? letter.upcase }
+	attempt.split('').all? {|letter| grid.include? letter.upcase}
 end
 
 def run_game(attempt, grid, start_time, end_time)
   #TODO: runs the game and return detailed hash of result
-  api_url = "http://api.wordreference.com/0.8/80143/json/enfr/" + attempt
-	open(api_url) do |stream| 
-	  response = JSON.parse(stream.read)
-	  if response['Error']
-	  	message = "Mot invalide"
-		else 
-			puts response['term0']['PrincipalTranslations']['0']['FirstTranslation']['term']
+
+
+
+  result = {
+  	message: "",
+  	translation: nil,
+  	time:end_time-start_time,
+  	score:0
+  }
+  if in_the_grid?(attempt,grid)
+	  api_url = "http://api.wordreference.com/0.8/80143/json/enfr/" + attempt
+		open(api_url) do |stream| 
+		  response = JSON.parse(stream.read)
+		  if response['Error']
+		  	result[:message] = "not an english word"
+			else 
+				result[:translation] = response['term0']['PrincipalTranslations']['0']['FirstTranslation']['term']
+				result[:score] = attempt.size/result[:time]
+				result[:message] = "well done"
+			end
 		end
-	  
-	  #puts response['term0']['PrincipalTranslations'][0]['FirstTranslation']
+	else
+		result[:message] = "not in the grid"
 	end
+
+	result
 end
